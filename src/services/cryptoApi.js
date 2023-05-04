a456c091c25e2c7849ed57be21d9357eeee5fdcf
Kerry Sabanty <kgsabanty@gmail.com>
2023-05-04 18:51:32 -0400

    loading cryptos onto the homepage

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,10 +2,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
   'X-RapidAPI-Key': '',
-  'X-RapidAPI-Host': ''
+  'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
 }
 
-const baseUrl = '';
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
